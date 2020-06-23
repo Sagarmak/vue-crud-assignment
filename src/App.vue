1e59e3da40f57f18f21e01325c83d350884ae2e9
Sagar Makhija <sagar1309@live.com>
2020-06-23 11:06:09 +0530

    completes View, Delete, Create and List Employees

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,100 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar />
+    <HomeComponent />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
+import HomeComponent from './components/HomeComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar,
+    HomeComponent
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*,
+*::after,
+*::before {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+  box-sizing: border-box;
+}
+
+body {
+  min-height: 100vh;
+  min-width: 100%;
+  line-height: 1;
+  font-family: 'Roboto', sans-serif;
+  font-size: 14px;
+  background: #fafafa;
+  color: #2b3945;
+}
+
+
+/* Helper */
+.container {
+  margin: 0 auto;
+  max-width: 80%;
+  padding: 2rem;
+  padding-bottom: 0;
+}
+
+/* Flexbox Helper */
+.flex {
+  display: flex;
+}
+.flex-column {
+  flex-direction: column;
+}
+.justify-center {
+  justify-content: center;
+}
+.justify-between {
+  justify-content: space-between;
+}
+.align-center {
+  align-items: center;
+}
+.flex-wrap {
+  flex-wrap: wrap;
+}
+
+.bold {
+  font-weight: bold;
+}
+.title {
+  font-size: 1.3rem;
+}
+.mr-2 {
+  margin-right: 10px;
+}
+button {
+  cursor: pointer;
+}
+.modal {
+  z-index: 1;
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background: rgb(0, 0, 0, 0.5);
+  overflow: auto;
+}
+.modal-content {
+  width: 85%;
+  margin: 2rem auto;
+  background: #fefefe;
 }
 </style>
